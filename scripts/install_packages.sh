38c4e8f3604a1149e499239ffda90cf5cf708d57
roudinebwt <roudinebwt@proton.me>
2025-12-24 04:37:42 +0100

    second round

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -83,7 +83,7 @@ dnf -y --enablerepo copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-l
 dnf -y copr enable bieszczaders/kernel-cachyos-addons
 dnf -y copr disable bieszczaders/kernel-cachyos-addons
 dnf -y --enablerepo copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-addons swap zram-generator-defaults cachyos-settings
-dnf -y --enablerepo copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-addons install \
+dnf -y --allowerasing --enablerepo copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-addons install \
   scx-scheds-git \
   scx-manager
 
