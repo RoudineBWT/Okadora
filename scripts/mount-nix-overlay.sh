2724d73cbd5899ffd61f06dcd0a2c67a39a64d7e
roudinebwt <roudinebwt@proton.me>
2025-12-26 00:01:55 +0100

    f6 for now

--- scripts/mount-nix-overlay.sh
+++ scripts/mount-nix-overlay.sh
@@ -1,11 +1,27 @@
-#!/usr/bin/bash
-set -e
+#!/bin/bash
 
-mkdir -p /usr/share/nix-store      
-mkdir -p /var/lib/nix-store        
-mkdir -p /var/cache/nix-store      
+set -euo pipefail
+
+
+if mountpoint -q /nix; then
+    echo "/nix is already mounted"
+    exit 0
+fi
+
+
+mkdir -p /usr/share/nix-store
+mkdir -p /var/lib/nix-store
+mkdir -p /var/cache/nix-store
 mkdir -p /nix
 
+
+if ! lsmod | grep -q overlay; then
+    modprobe overlay
+fi
+
+# Monter l'overlay
 mount -t overlay overlay \
-  -o lowerdir=/usr/share/nix-store,upperdir=/var/lib/nix-store,workdir=/var/cache/nix-store \
-  /nix
+    -o lowerdir=/usr/share/nix-store,upperdir=/var/lib/nix-store,workdir=/var/cache/nix-store \
+    /nix
+
+echo "OverlayFS for /nix mounted successfully"
